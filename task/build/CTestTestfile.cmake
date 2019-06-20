0a194f6a1cdaa09bc5a3723063ac86c046ac9d6f
Anupam Kaul <anupam.kaul@yahoo.com>
2019-06-20 13:42:55 -0700

    Structural changes for a separate repository
    Tested that launch.sh works and launches all terminals
    Saw RTAB window working when I used teleop to move my robot
    Also saw in rviz that graph-slam was working

---
+++ task/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/anupam/robot_slam/task/src
+# Build directory: /home/anupam/robot_slam/task/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(my_robot)
+subdirs(teleop_twist_keyboard)
